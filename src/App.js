fe1de82de4fa10947da3335a4ce83ab10dc6a3ff
Cory House <housecor@gmail.com>
2020-05-17 13:57:09 -0500

    format

--- src/App.js
+++ src/App.js
@@ -49,15 +49,19 @@ function App() {
   return (
     <div className="crf">
       <Nav cart={cart} />
+
       <Route path="/" exact>
         <Home shoes={shoes} />
       </Route>
+
       <Route path="/shoe/:id">
         <ShoeDetail cart={cart} shoes={shoes} addToCart={addToCart} />
       </Route>
+
       <Route path="/cart">
         <Cart cart={cart} shoes={shoes} removeFromCart={removeFromCart} />
       </Route>
+
       <Footer />
     </div>
   );
