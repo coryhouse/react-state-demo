38addca3ef8a93ea7b58e8473400eda47f97e0b0
Cory House <housecor@gmail.com>
2020-05-17 13:48:12 -0500

    Add desc

--- src/services/shoeApi.js
+++ src/services/shoeApi.js
@@ -1,13 +1,28 @@
 export async function getShoes() {
   return [
-    { id: 1, name: "Hiker", price: 94.95, width: ["M"], sizes: [7, 8] },
-    { id: 2, name: "Climber", price: 78.99, width: ["M"], sizes: [8, 9] },
+    {
+      id: 1,
+      name: "Hiker",
+      price: 94.95,
+      width: ["M"],
+      sizes: [7, 8],
+      description: "This rugged shoe will get you up the mountain safely.",
+    },
+    {
+      id: 2,
+      name: "Climber",
+      price: 78.99,
+      width: ["M"],
+      sizes: [8, 9],
+      description: "Sure-footed traction in slippery conditions.",
+    },
     {
       id: 3,
       name: "Explorer",
       price: 145.95,
       width: ["N", "M", "W"],
       sizes: [7, 8, 9],
+      description: "Look stylish while stomping in the mud.",
     },
   ];
 }
