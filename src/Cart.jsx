f5727143c7b4f7c554a2c2bdee8294c987e5dfe6
Cory House <housecor@gmail.com>
2020-05-17 16:50:36 -0500

    Handle empty cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -7,43 +7,45 @@ export default function Cart({ cart, shoes, removeFromCart }) {
   return (
     <section id="cart">
       <h1>Cart</h1>
+      {cart.length === 0 && <p>Your cart is empty.</p>}
       <p>
         <Link to="/">Continue Shopping</Link>
       </p>
-      <table>
-        <thead>
-          <th></th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Total</th>
-        </thead>
-        <tbody>
-          {cart.map((id) => {
-            const shoe = shoes.find((s) => s.id === id);
-            return (
-              <tr>
-                <td>
-                  <button
-                    className="btn btn-link"
-                    onClick={() => removeFromCart(shoe.id)}
-                  >
-                    Remove
-                  </button>
-                  <img src={`/images/shoe${shoe.id}.jpg`} alt="shoe" />
-                </td>
-                <td>${shoe.price}</td>
-                <td>
-                  <input type="number" onChange={setQuantity} />
-                </td>
-                <td>
-                  <strong>${shoe.price * 1}</strong>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <p></p>
+      {cart.length > 0 && (
+        <table>
+          <thead>
+            <th></th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </thead>
+          <tbody>
+            {cart.map((id) => {
+              const shoe = shoes.find((s) => s.id === id);
+              return (
+                <tr>
+                  <td>
+                    <button
+                      className="btn btn-link"
+                      onClick={() => removeFromCart(shoe.id)}
+                    >
+                      Remove
+                    </button>
+                    <img src={`/images/shoe${shoe.id}.jpg`} alt="shoe" />
+                  </td>
+                  <td>${shoe.price}</td>
+                  <td>
+                    <input type="number" onChange={setQuantity} />
+                  </td>
+                  <td>
+                    <strong>${shoe.price * 1}</strong>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </section>
   );
 }
