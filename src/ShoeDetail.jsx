c1df1412a80a399721319961cb458917d4045be7
Cory House <housecor@gmail.com>
2020-05-17 16:18:22 -0500

    size qty input

--- src/ShoeDetail.jsx
+++ src/ShoeDetail.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouteMatch, Link } from "react-router-dom";
+import SelectSize from "./SelectSize";
 
 export default function ShoeDetail({ shoes, cart, addToCart }) {
+  const [size, setSize] = useState(null);
   const { params } = useRouteMatch();
   const shoe = shoes.find((shoe) => shoe.id === parseInt(params["id"]));
 
@@ -11,17 +13,28 @@ export default function ShoeDetail({ shoes, cart, addToCart }) {
       <p>{shoe.description}</p>
       <h2>${shoe.price}</h2>
       <p>
+        <SelectSize
+          onChange={(e) => setSize(e.target.value)}
+          value={size}
+          defaultOptionLabel="What size?"
+        />
+      </p>
+      <p>
         {cart.find((c) => c === shoe.id) ? (
           <Link to="/cart">In Cart</Link>
         ) : (
-          <button onClick={() => addToCart(shoe.id)}>Add to cart</button>
+          <button
+            className="btn btn-primary"
+            disabled={!size}
+            onClick={() => addToCart(shoe.id)}
+          >
+            Add to cart
+          </button>
         )}
       </p>
-
       <p>
         <Link to="/">Go Back</Link>
       </p>
-
       <img src={`/images/shoe${shoe.id}.jpg`} alt="shoe" />
     </>
   );
