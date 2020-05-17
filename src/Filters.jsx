dd55ec42dba5661746db47ef9eb41986e3100c8a
Cory House <housecor@gmail.com>
2020-05-17 13:13:37 -0500

    Streamline

--- src/Filters.jsx
+++ src/Filters.jsx
@@ -2,16 +2,9 @@ import React, { useState } from "react";
 
 export default function Filters() {
   const [size, setSize] = useState(localStorage.getItem("size"));
-  const [width, setWidth] = useState(null);
 
   return (
     <section>
-      {/* <label htmlFor="width">Width</label>
-      <select id="width" value={width}>
-        {["S", "M", "L"].map((w) => (
-          <option value={w}>{w}</option>
-        ))}
-      </select> */}
       <label htmlFor="size">Size:</label>{" "}
       <select id="size" value={size} onChange={(e) => setSize(e.target.value)}>
         <option></option>
