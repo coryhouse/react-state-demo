449c6d01a8fdcf9bf7631a79cfee634a510a3571
Cory House <housecor@gmail.com>
2020-05-23 10:39:24 -0500

    Refactor to derive state

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -11,9 +11,8 @@ const newAddress = {
 
 const STATUS = {
   IDLE: "IDLE",
-  DIRTY: "DIRTY",
-  SUBMITTING: "SUBMITTING",
   SUBMITTED: "SUBMITTED",
+  SUBMITTING: "SUBMITTING",
 };
 
 export default function Checkout({ emptyCart }) {
@@ -21,13 +20,18 @@ export default function Checkout({ emptyCart }) {
   // Point: When to split vs unify state.
   // Tradeoff: unifying makes it easier to send to server, but slightly more work to update.
   const [address, setAddress] = useState(newAddress);
-  const [errors, setErrors] = useState({});
+  // Object with property for each field that has been touched.
+  const [touched, setTouched] = useState({});
   const [status, setStatus] = useState(STATUS.IDLE);
+
+  // Derived state
+  const dirty = address !== newAddress;
+  const errors = getErrors(address);
+  const isValid = Object.keys(errors).length === 0;
 
   // Show controlled vs uncontrolled form.
   function handleChange(e) {
     e.persist();
-    if (status === STATUS.IDLE) setStatus(STATUS.DIRTY);
     // Using callback form of setter here since we need the existing state
     setAddress((curAddress) => {
       // Note that we're storing the new data here and passing to validate. Otherwise, validate would use stale data since setting state is async.
@@ -35,24 +39,19 @@ export default function Checkout({ emptyCart }) {
         ...curAddress,
         [e.target.id]: e.target.value,
       };
-      if (status === STATUS.SUBMITTED) validate(updatedAddress);
       return updatedAddress;
     });
   }
 
-  // Returns true if the form is valid
-  function validate(address) {
-    const _errors = {};
-    if (!address.city) _errors.city = "City is required.";
-    if (!address.country) _errors.country = "Country is required.";
-    setErrors(_errors);
-    // Note that if I read from state after calling this, it won't work because set state is async.
-    return Object.keys(_errors).length === 0;
+  function getErrors(address) {
+    const errors = {};
+    if (!address.city) errors.city = "City is required.";
+    if (!address.country) errors.country = "Country is required.";
+    return errors;
   }
 
   async function handleSubmit(event) {
     event.preventDefault();
-    const isValid = validate(address);
     if (isValid) {
       setStatus(STATUS.SUBMITTING);
       await saveShipping(address);
@@ -63,12 +62,14 @@ export default function Checkout({ emptyCart }) {
     }
   }
 
-  const errorsExist = Object.keys(errors).length > 0;
+  function handleBlur(event) {
+    setTouched({ ...touched, [event.target.id]: true });
+  }
 
   return (
     <>
       <h1>Shipping Info</h1>
-      {errorsExist && (
+      {!isValid && status === STATUS.SUBMITTED && (
         <ul>
           {Object.keys(errors).map((key) => {
             return <li>{errors[key]}</li>;
@@ -82,15 +83,23 @@ export default function Checkout({ emptyCart }) {
           <input
             id="city"
             type="text"
+            onBlur={handleBlur}
             value={address.city}
             onChange={handleChange}
           />
         </div>
 
+        <p>{touched.city && errors.city}</p>
+
         <div>
           <label htmlFor="country">Country</label>
           <br />
-          <select id="country" value={address.country} onChange={handleChange}>
+          <select
+            onBlur={handleBlur}
+            id="country"
+            value={address.country}
+            onChange={handleChange}
+          >
             <option>Select Country</option>
             <option value="China">China</option>
             <option value="India">India</option>
@@ -99,12 +108,14 @@ export default function Checkout({ emptyCart }) {
           </select>
         </div>
 
+        <p>{touched.country && errors.country}</p>
+
         <div>
           <input
             type="submit"
             className="btn btn-primary"
             value="Save Shipping Info"
-            disabled={errorsExist || status === STATUS.SUBMITTING}
+            disabled={status === STATUS.SUBMITTING}
           />
         </div>
       </form>
