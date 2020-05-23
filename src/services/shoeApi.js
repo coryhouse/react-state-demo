bbcee702bfc0c8131bb43ae6c3a6e1c32e941456
Cory House <housecor@gmail.com>
2020-05-23 15:30:24 -0500

    Fix api service

--- src/services/shoeApi.js
+++ src/services/shoeApi.js
@@ -1,39 +1,17 @@
+const baseUrl = "http://localhost:3001/";
+
 export async function getShoes() {
-  return [
-    {
-      id: 1,
-      name: "Hiker",
-      price: 94.95,
-      width: ["M"],
-      sizes: [7, 8],
-      description: "This rugged boot will get you up the mountain safely.",
-    },
-    {
-      id: 2,
-      name: "Climber",
-      price: 78.99,
-      width: ["M"],
-      sizes: [8, 9],
-      description: "Sure-footed traction in slippery conditions.",
-    },
-    {
-      id: 3,
-      name: "Explorer",
-      price: 145.95,
-      width: ["N", "M", "W"],
-      sizes: [7, 8, 9],
-      description: "Look stylish while stomping in the mud.",
-    },
-  ];
+  return fetch(baseUrl + "shoes").then((response) => {
+    if (response.ok) return response.json();
+  });
 }
 
-export async function saveShipping(shippingInfo) {
-  return await setTimeout(() => {
-    console.log("saving shipping info", shippingInfo);
-    return new Promise((resolve) => {
-      resolve({
-        statusCode: 200,
-      });
-    });
-  }, 2000);
+export async function saveShippingAddress(address) {
+  return fetch(baseUrl + "shippingAddress", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(address),
+  });
 }
