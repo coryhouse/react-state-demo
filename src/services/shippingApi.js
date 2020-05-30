40339a4e48e34d3121e8f7a70017fd58818b7086
Cory House <housecor@gmail.com>
2020-05-30 08:39:49 -0500

    extract shipping api

---
+++ src/services/shippingApi.js
@@ -0,0 +1,17 @@
+const baseUrl = "http://localhost:3001/";
+
+export async function getShippingAddress(userId) {
+    return fetch(baseUrl + "shippingAddress/" + userId).then((response) => {
+      if (response.ok) return response.json();
+    });
+  }
+  
+  export async function saveShippingAddress(address) {
+    return fetch(baseUrl + "shippingAddress", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(address),
+    });
+  }
--- src/services/shoeApi.js
+++ src/services/shoeApi.js
@@ -12,12 +12,4 @@ export async function getShoe(key, shoeId) {
   });
 }
 
-export async function saveShippingAddress(address) {
-  return fetch(baseUrl + "shippingAddress", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(address),
-  });
-}
+
