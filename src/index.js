e5604dbeeac22565cb242e8e052de6463b95d5b2
Cory House <housecor@gmail.com>
2020-05-30 08:47:03 -0500

    Add devtools

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
