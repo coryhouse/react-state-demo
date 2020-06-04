1f0897dc167787ed13931dcef271479b9353cd0e
Cory House <housecor@gmail.com>
2020-06-04 09:52:54 -0500

    Refactor

--- src/App.js
+++ src/App.js
@@ -38,11 +38,10 @@ function App() {
     setCart((cart) => {
       const alreadyInCart = cart.find((i) => i.id === id && i.size === size);
       if (alreadyInCart)
-        return cart.map((i) =>
-          i.id === id && i.size === size
-            ? { ...i, quantity: i.quantity + 1 }
-            : i
-        );
+        return cart.map((i) => {
+          const isMatchingItem = i.id === id && i.size === size;
+          return isMatchingItem ? { ...i, quantity: i.quantity + 1 } : i;
+        });
       return [...cart, { id, size, quantity: 1 }];
     });
     history.push("/cart");
