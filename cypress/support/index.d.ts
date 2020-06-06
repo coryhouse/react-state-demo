5543feed7073c891fd3bf184de92f34829a42172
Cory House <housecor@gmail.com>
2020-06-06 08:04:16 -0500

    Add type

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+// As mentioned here: https://docs.cypress.io/api/cypress-api/custom-commands.html#5-Write-TypeScript-definitions
+declare namespace Cypress {
+  interface Chainable<Subject> {
+    /**
+     * Add a shoe to the cart
+     * @example
+     * cy.addToCart(id, size)
+     */
+    addToCart(id: number, size: string): Chainable<any>;
+  }
+}
