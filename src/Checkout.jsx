ec54b2095049065b93e30f480cb2b634299f6abb
Cory House <housecor@gmail.com>
2020-06-11 10:26:52 -0500

    Implement ref example

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { saveShippingAddress } from "./services/shippingApi";
 
 // Declare static data outside the component to avoid needless recreation on each render.
 // Challenge: Finish building out the checkout with credit card, billing address, totals.
-const newAddress = {
-  city: "",
-  country: "",
-};
 
 const STATUS = {
   IDLE: "IDLE",
@@ -17,42 +13,37 @@ const STATUS = {
 
 export default function Checkout({ emptyCart }) {
   const history = useHistory();
+  const cityRef = useRef();
+  const countryRef = useRef();
+
   // Point: When to split vs unify state.
   // Tradeoff: unifying makes it easier to send to server, but slightly more work to update.
-  const [address, setAddress] = useState(newAddress);
+  const [errors, setErrors] = useState({});
   // Object with property for each field that has been touched.
-  const [touched, setTouched] = useState({});
   const [status, setStatus] = useState(STATUS.IDLE);
 
   // Derived state
-  const dirty = address !== newAddress;
-  const errors = getErrors(address);
-  const isValid = Object.keys(errors).length === 0;
-
-  // Show controlled vs uncontrolled form.
-  function handleChange(e) {
-    e.persist();
-    // Using callback form of setter here since we need the existing state
-    setAddress((curAddress) => {
-      // Note that we're storing the new data here and passing to validate. Otherwise, validate would use stale data since setting state is async.
-      const updatedAddress = {
-        ...curAddress,
-        [e.target.id]: e.target.value,
-      };
-      return updatedAddress;
-    });
-  }
+  const isValid = () => Object.keys(errors).length === 0;
 
   function getErrors(address) {
-    const errors = {};
-    if (!address.city) errors.city = "City is required.";
-    if (!address.country) errors.country = "Country is required.";
-    return errors;
+    const _errors = {};
+    if (!address.city) _errors.city = "City is required.";
+    if (!address.country) _errors.country = "Country is required.";
+    debugger;
+    setErrors(_errors);
+    return _errors;
   }
 
   async function handleSubmit(event) {
     event.preventDefault();
-    if (isValid) {
+    const address = {
+      city: cityRef.current.value,
+      country: countryRef.current.value,
+    };
+    const errors = getErrors(address);
+
+    // Have to do this here because state updates are async, so can't rely on reading state here.
+    if (Object.keys(errors).length === 0) {
       setStatus(STATUS.SUBMITTING);
       await saveShippingAddress(address);
       emptyCart();
@@ -62,14 +53,10 @@ export default function Checkout({ emptyCart }) {
     }
   }
 
-  function handleBlur(event) {
-    setTouched({ ...touched, [event.target.id]: true });
-  }
-
   return (
     <>
       <h1>Shipping Info</h1>
-      {!isValid && status === STATUS.SUBMITTED && (
+      {!isValid() && status === STATUS.SUBMITTED && (
         <div role="alert">
           <p>Please fix the following errors:</p>
           <ul>
@@ -83,38 +70,23 @@ export default function Checkout({ emptyCart }) {
         <div>
           <label htmlFor="city">City</label>
           <br />
-          <input
-            id="city"
-            type="text"
-            onBlur={handleBlur}
-            value={address.city}
-            onChange={handleChange}
-          />
+          <input ref={cityRef} id="city" type="text" />
 
-          <p role="alert">
-            {(touched.city || status === STATUS.SUBMITTED) && errors.city}
-          </p>
+          <p role="alert">{status === STATUS.SUBMITTED && errors.city}</p>
         </div>
 
         <div>
           <label htmlFor="country">Country</label>
           <br />
-          <select
-            onBlur={handleBlur}
-            id="country"
-            value={address.country}
-            onChange={handleChange}
-          >
-            <option>Select Country</option>
+          <select ref={countryRef} id="country" defaultValue="USA">
+            <option value="">Select Country</option>
             <option value="China">China</option>
             <option value="India">India</option>
             <option value="United Kingodom">United Kingdom</option>
             <option value="USA">USA</option>
           </select>
 
-          <p role="alert">
-            {(touched.country || status === STATUS.SUBMITTED) && errors.country}
-          </p>
+          <p role="alert">{status === STATUS.SUBMITTED && errors.country}</p>
         </div>
 
         <div>
