b04b8f0ded942fa24e9af390a13ea837a8425b96
Cory House <housecor@gmail.com>
2020-06-20 13:31:08 -0500

    Convert to class

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React from "react";
 import { saveShippingAddress } from "./services/shippingApi";
 
 // Declare static data outside the component to avoid needless recreation on each render.
@@ -15,117 +14,133 @@ const STATUS = {
   SUBMITTING: "SUBMITTING",
 };
 
-export default function Checkout({ emptyCart }) {
-  const history = useHistory();
-  // Point: When to split vs unify state.
-  // Tradeoff: unifying makes it easier to send to server, but slightly more work to update.
-  const [address, setAddress] = useState(newAddress);
-  // Object with property for each field that has been touched.
-  const [touched, setTouched] = useState({});
-  const [status, setStatus] = useState(STATUS.IDLE);
+export default class Checkout extends React.Component {
+  constructor(props) {
+    super(props);
 
-  // Derived state
-  const dirty = address !== newAddress;
-  const errors = getErrors(address);
-  const isValid = Object.keys(errors).length === 0;
-
-  // Show controlled vs uncontrolled form.
-  function handleChange(e) {
-    e.persist();
-    // Using callback form of setter here since we need the existing state
-    setAddress((curAddress) => {
-      // Note that we're storing the new data here and passing to validate. Otherwise, validate would use stale data since setting state is async.
-      const updatedAddress = {
-        ...curAddress,
-        [e.target.id]: e.target.value,
-      };
-      return updatedAddress;
-    });
+    this.state = {
+      address: newAddress,
+      touched: {},
+      status: STATUS.IDLE,
+    };
   }
 
-  function getErrors(address) {
+  // Derived state
+  dirty = () => this.state.address !== newAddress;
+  errors = () => this.getErrors(this.state.address);
+  isValid = () => Object.keys(this.errors()).length === 0;
+
+  // Show controlled vs uncontrolled form.
+  handleChange = (e) => {
+    e.persist();
+    // Using callback form of setter here since we need the existing state
+    this.setState((curState) => {
+      // Note that we're storing the new data here and passing to validate. Otherwise, validate would use stale data since setting state is async.
+      const address = {
+        ...curState.address,
+        [e.target.id]: e.target.value,
+      };
+      return { address };
+    });
+  };
+
+  getErrors(address) {
     const errors = {};
     if (!address.city) errors.city = "City is required.";
     if (!address.country) errors.country = "Country is required.";
     return errors;
   }
 
-  async function handleSubmit(event) {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    if (isValid) {
-      setStatus(STATUS.SUBMITTING);
-      await saveShippingAddress(address);
-      emptyCart();
-      history.push("/confirmation");
+    if (this.isValid()) {
+      this.setState({ status: STATUS.SUBMITTING });
+      await saveShippingAddress(this.state.address);
+      this.props.emptyCart();
+      this.props.history.push("/confirmation");
     } else {
-      setStatus(STATUS.SUBMITTED);
+      this.setState({ status: STATUS.SUBMITTED });
     }
+  };
+
+  handleBlur = (event) => {
+    event.persist();
+    this.setState((curState) => {
+      return {
+        touched: {
+          ...curState.touched,
+          [event.target.id]: true,
+        },
+      };
+    });
+  };
+
+  render() {
+    const errors = this.errors();
+    const { address, touched, status } = this.state;
+
+    return (
+      <>
+        <h1>Shipping Info</h1>
+        {!this.isValid() && this.state.status === STATUS.SUBMITTED && (
+          <div role="alert">
+            <p>Please fix the following errors:</p>
+            <ul>
+              {Object.keys(errors).map((key) => (
+                <li key={key}>{errors[key]}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="city">City</label>
+            <br />
+            <input
+              id="city"
+              type="text"
+              onBlur={this.handleBlur}
+              value={address.city}
+              onChange={this.handleChange}
+            />
+
+            <p role="alert">
+              {(touched.city || status === STATUS.SUBMITTED) && errors.city}
+            </p>
+          </div>
+
+          <div>
+            <label htmlFor="country">Country</label>
+            <br />
+            <select
+              onBlur={this.handleBlur}
+              id="country"
+              value={address.country}
+              onChange={this.handleChange}
+            >
+              <option>Select Country</option>
+              <option value="China">China</option>
+              <option value="India">India</option>
+              <option value="United Kingodom">United Kingdom</option>
+              <option value="USA">USA</option>
+            </select>
+
+            <p role="alert">
+              {(touched.country || status === STATUS.SUBMITTED) &&
+                errors.country}
+            </p>
+          </div>
+
+          <div>
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value="Save Shipping Info"
+              disabled={status === STATUS.SUBMITTING}
+            />
+          </div>
+        </form>
+      </>
+    );
   }
-
-  function handleBlur(event) {
-    setTouched({ ...touched, [event.target.id]: true });
-  }
-
-  return (
-    <>
-      <h1>Shipping Info</h1>
-      {!isValid && status === STATUS.SUBMITTED && (
-        <div role="alert">
-          <p>Please fix the following errors:</p>
-          <ul>
-            {Object.keys(errors).map((key) => {
-              return <li key={key}>{errors[key]}</li>;
-            })}
-          </ul>
-        </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="city">City</label>
-          <br />
-          <input
-            id="city"
-            type="text"
-            onBlur={handleBlur}
-            value={address.city}
-            onChange={handleChange}
-          />
-
-          <p role="alert">
-            {(touched.city || status === STATUS.SUBMITTED) && errors.city}
-          </p>
-        </div>
-
-        <div>
-          <label htmlFor="country">Country</label>
-          <br />
-          <select
-            onBlur={handleBlur}
-            id="country"
-            value={address.country}
-            onChange={handleChange}
-          >
-            <option>Select Country</option>
-            <option value="China">China</option>
-            <option value="India">India</option>
-            <option value="United Kingodom">United Kingdom</option>
-            <option value="USA">USA</option>
-          </select>
-
-          <p role="alert">
-            {(touched.country || status === STATUS.SUBMITTED) && errors.country}
-          </p>
-        </div>
-
-        <div>
-          <input
-            type="submit"
-            className="btn btn-primary"
-            value="Save Shipping Info"
-            disabled={status === STATUS.SUBMITTING}
-          />
-        </div>
-      </form>
-    </>
-  );
 }
