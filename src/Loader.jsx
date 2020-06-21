ff20204eda5660ff6bc1b87bc1b5a1cee1772da2
Cory House <housecor@gmail.com>
2020-06-21 15:47:31 -0500

    Add loader

---
+++ src/Loader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Loader.css";
+
+export default function Loader() {
+  return (
+    <div className="lds-container">
+      <div className="lds-dual-ring"></div>
+    </div>
+  );
+}
