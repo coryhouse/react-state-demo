dbb28576acc763e7ff104bbcf82a2a87d8ad5dc7
Cory House <housecor@gmail.com>
2020-06-21 14:44:52 -0500

    only run once

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -5,6 +5,7 @@ export default function useFetch(url, init) {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (data || error) return; // Only fetch once
     fetch(process.env.REACT_APP_API_BASE_URL + url, init)
       .then((response) => {
         if (response.ok) return response.json();
@@ -15,7 +16,7 @@ export default function useFetch(url, init) {
         console.error(err);
         setError(err);
       });
-  }, [init, url]);
+  }, [data, error, init, url]);
 
   return [data, error];
 }
--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -5,6 +5,7 @@ export default function useFetchAll(requests) {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (data || error) return; // Only run once
     const promises = requests.map(({ url, init }) =>
       fetch(process.env.REACT_APP_API_BASE_URL + url, init)
     );
@@ -22,7 +23,7 @@ export default function useFetchAll(requests) {
         console.error(err);
         setError(err);
       });
-  }, [requests]);
+  }, [data, error, requests]);
 
   return [data, error];
 }
