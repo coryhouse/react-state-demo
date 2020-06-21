1ee676eb7edb478e6e7549644bea825e712a23b0
Cory House <housecor@gmail.com>
2020-06-21 09:00:22 -0500

    Simplify

--- src/services/useProductDetailService.js
+++ src/services/useProductDetailService.js
@@ -3,14 +3,12 @@ import { getProduct } from "./productService";
 
 export default function useProductDetailService(productIds) {
   const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     Promise.all(productIds.map((id) => getProduct(id))).then((response) => {
       setProducts(response);
-      setLoading(false);
     });
   }, [productIds]);
 
-  return { products, loading };
+  return [products];
 }
