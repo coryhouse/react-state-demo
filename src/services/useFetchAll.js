8a09b7a8f311053e65a615f4d25e38d2ea7278c9
Cory House <housecor@gmail.com>
2020-06-21 16:03:26 -0500

    Refactor

--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -7,17 +7,12 @@ export default function useFetchAll(requests) {
   useEffect(() => {
     if (data ?? error) return; // Only run once
     const promises = requests.map(({ url, init }) =>
-      fetch(process.env.REACT_APP_API_BASE_URL + url, init)
+      fetch(process.env.REACT_APP_API_BASE_URL + url, init).then((response) => {
+        if (response.ok) return response.json();
+        throw new Error("Bad network response");
+      })
     );
     Promise.all(promises)
-      .then((responses) => {
-        return Promise.all(
-          responses.map((response) => {
-            if (!response.ok) setError("Bad network response.");
-            return response.json();
-          })
-        );
-      })
       .then((data) => setData(data))
       .catch((err) => {
         console.error(err);
