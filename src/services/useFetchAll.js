53a3239f555a320d8b9e0837f2bec0c3ceab1690
Cory House <housecor@gmail.com>
2020-06-21 16:03:37 -0500

    lines

--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -6,12 +6,14 @@ export default function useFetchAll(requests) {
 
   useEffect(() => {
     if (data ?? error) return; // Only run once
+
     const promises = requests.map(({ url, init }) =>
       fetch(process.env.REACT_APP_API_BASE_URL + url, init).then((response) => {
         if (response.ok) return response.json();
         throw new Error("Bad network response");
       })
     );
+
     Promise.all(promises)
       .then((data) => setData(data))
       .catch((err) => {
