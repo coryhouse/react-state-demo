8ec4f1971d4854d738eb64a97498b1bacb51dede
Cory House <housecor@gmail.com>
2020-06-21 09:30:47 -0500

    Switch to useFetchAll

---
+++ src/services/useFetchAll.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+export default function useFetchAll(requests) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const promises = requests.map(({ url, init }) =>
+      fetch(process.env.REACT_APP_API_BASE_URL + url, init)
+    );
+    Promise.all(promises)
+      .then((responses) => {
+        return Promise.all(
+          responses.map((response) => {
+            if (response.ok) return response.json();
+            setError("Bad network response.");
+          })
+        );
+      })
+      .then((data) => setData(data))
+      .catch((err) => {
+        console.error(err);
+        setError(err);
+      });
+  }, [requests]);
+
+  return [data, error];
+}
