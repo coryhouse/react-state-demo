9f639998079db64990bf9369e679c3e9c81c46c4
Cory House <housecor@gmail.com>
2020-06-30 13:39:39 -0500

    Add loading state. Re-run when params change

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,11 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function useFetch(url, init) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const prevInit = useRef();
+  const prevUrl = useRef();
 
   useEffect(() => {
-    if (data ?? error) return; // Only fetch once
+    // Only refetch if url or init params change.
+    if (prevUrl.current === url && prevInit.current === init) return;
+    prevUrl.current = url;
+    prevInit.current = init;
     fetch(process.env.REACT_APP_API_BASE_URL + url, init)
       .then((response) => {
         if (response.ok) return response.json();
@@ -15,8 +21,9 @@ export default function useFetch(url, init) {
       .catch((err) => {
         console.error(err);
         setError(err);
-      });
-  }, [data, error, init, url]);
+      })
+      .finally(() => setLoading(false));
+  }, [init, url]);
 
-  return [data, error];
+  return [data, loading, error];
 }
