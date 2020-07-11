490385e72b7f1c0cb2d8739138c127f2f6fc8e6c
Cory House <housecor@gmail.com>
2020-07-10 21:35:42 -0500

    Rename

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -22,7 +22,7 @@ export default function Cart({ cart, setCart }) {
 
   function renderItem(item) {
     const { id, size, quantity } = item;
-    const { price, name, image } = products.find((s) => s.id === item.id);
+    const { price, name, image } = products.find((p) => p.id === id);
     return (
       <li key={id + size} className="cart-item">
         <img src={`/images/${image}`} alt={name} />
