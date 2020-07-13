f9bbc11ac8db5e1c8ce7136e26f5d2b6453f5317
Cory House <housecor@gmail.com>
2020-07-13 17:43:22 -0500

    Add dep check

--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -1,11 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function useFetchAll(urls) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const prevUrls = useRef([]);
+
+  function areEqual(array1, array2) {
+    return (
+      array1.length === array2.length &&
+      array1.every((value, index) => value === array2[index])
+    );
+  }
 
   useEffect(() => {
+    // Only re-run if the array of URLs passed in changes
+    if (areEqual(prevUrls.current, urls)) return;
+    prevUrls.current = urls;
     const promises = urls.map((url) =>
       fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
         if (response.ok) return response.json();
@@ -20,8 +31,7 @@ export default function useFetchAll(urls) {
         setError(err);
       })
       .finally(() => setLoading(false));
-    // eslint-disable-next-line
-  }, []);
+  }, [urls]);
 
   return [data, loading, error];
 }
