5e8ba6cb419f05bf0d0ca390f073f899f6cc70d3
Cory House <housecor@gmail.com>
2020-07-12 19:55:53 -0500

    Remove useless checks

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,17 +1,11 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 export default function useFetch(url, init) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const prevInit = useRef();
-  const prevUrl = useRef();
 
   useEffect(() => {
-    // Only refetch if url or init params change.
-    if (prevUrl.current === url && prevInit.current === init) return;
-    prevUrl.current = url;
-    prevInit.current = init;
     fetch(process.env.REACT_APP_API_BASE_URL + url, init)
       .then((response) => {
         if (response.ok) return response.json();
